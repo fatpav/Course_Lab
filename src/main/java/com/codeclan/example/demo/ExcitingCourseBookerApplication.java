b9e23ccd12ed1f9d9e8e2a45afd36174f1527089
fatpav <fat_pav@hotmail.com>
2020-12-03 10:25:09 +0000

    initial commit

---
+++ src/main/java/com/codeclan/example/demo/ExcitingCourseBookerApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcitingCourseBookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcitingCourseBookerApplication.class, args);
+	}
+
+}
