2f0f0a29972fd60f6bfb680ff717c8f530f04fed
yanazykova <yvz1973@gmail.com>
2020-12-03 11:21:58 +0000

    Set up Customer class

--- src/main/java/com/codeclan/example/demo/models/Customer.java
+++ src/main/java/com/codeclan/example/demo/models/Customer.java
@@ -1,4 +1,79 @@
 package com.codeclan.example.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Column(name = "name")
+    private String name;
+    @Column(name = "town")
+    private String town;
+    @Column(name = "age")
+    private int age;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    private List<Booking> bookings;
+
+    @JsonIgnoreProperties({"customers"})
+    @ManyToMany
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @JoinTable(
+        name = "customers_courses",
+        joinColumns = {@JoinColumn(name = "customer_id", nullable = false, updatable = false)},
+        inverseJoinColumns = {@JoinColumn(name = "course_id", nullable = false, updatable = false)},
+        )
+        private List<Course> courses;
+
+    public Customer(String name, String town, int age) {
+        this.name = name;
+        this.town = town;
+        this.age = age;
+    }
+
+    public Customer(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
