4656d75260758f2a9fd4db08f947681d173e9018
yanazykova <yvz1973@gmail.com>
2020-12-03 11:09:13 +0000

    Set up Booking class

--- src/main/java/com/codeclan/example/demo/models/Booking.java
+++ src/main/java/com/codeclan/example/demo/models/Booking.java
@@ -1,4 +1,52 @@
 package com.codeclan.example.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bookings")
 public class Booking {
+
+
+    @Column(name = "date")
+    private int date;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JsonIgnoreProperties({"bookings"})
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @JsonIgnoreProperties({"bookings"})
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    public Booking(int date) {
+        this.date = date;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Booking() {
+
+    }
+
 }
