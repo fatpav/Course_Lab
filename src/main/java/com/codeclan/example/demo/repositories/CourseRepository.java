7a6be436b7dc905ceaaf8224ef3a8ec846ae94c4
yanazykova <yvz1973@gmail.com>
2020-12-03 14:23:54 +0000

    Added filter course by rating

--- src/main/java/com/codeclan/example/demo/repositories/CourseRepository.java
+++ src/main/java/com/codeclan/example/demo/repositories/CourseRepository.java
@@ -3,5 +3,9 @@ package com.codeclan.example.demo.repositories;
 import com.codeclan.example.demo.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+//    Get all courses with a given rating
+    List<Course> findCourseByRating (int rating);
 }
