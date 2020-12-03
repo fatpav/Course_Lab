d8c89f363bb865e57437eac89553f12a01f3ab5e
yanazykova <yvz1973@gmail.com>
2020-12-03 10:49:56 +0000

    Set up Course class

--- src/main/java/com/codeclan/example/demo/models/Course.java
+++ src/main/java/com/codeclan/example/demo/models/Course.java
@@ -1,4 +1,69 @@
 package com.codeclan.example.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="course")
 public class Course {
+    @Column(name="course_name")
+    private String courseName;
+    @Column(name="town")
+    private String town;
+    @Column(name="star_rating")
+    private int starRating;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    private List<Booking> bookings;
+
+
+    public Course(String courseName, String town, int starRating) {
+        this.courseName = courseName;
+        this.town = town;
+        this.starRating = starRating;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(int starRating) {
+        this.starRating = starRating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Course(){
+
+    }
 }
