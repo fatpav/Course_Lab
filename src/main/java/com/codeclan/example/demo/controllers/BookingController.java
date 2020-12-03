08247c440502d65ddef1f87498ae1190ba846a5c
fatpav <fat_pav@hotmail.com>
2020-12-03 13:23:50 +0000

    customer controller

--- src/main/java/com/codeclan/example/demo/controllers/BookingController.java
+++ src/main/java/com/codeclan/example/demo/controllers/BookingController.java
@@ -1,4 +1,12 @@
 package com.codeclan.example.demo.controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookingController {
+
+    @Autowired
+
 }
--- src/test/java/com/codeclan/example/demo/ExcitingCourseBookerApplicationTests.java
+++ src/test/java/com/codeclan/example/demo/ExcitingCourseBookerApplicationTests.java
@@ -1,13 +1,41 @@
 package com.codeclan.example.demo;
 
+import com.codeclan.example.demo.models.Customer;
+import com.codeclan.example.demo.repositories.BookingRepository;
+import com.codeclan.example.demo.repositories.CourseRepository;
+import com.codeclan.example.demo.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ExcitingCourseBookerApplicationTests {
 
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	BookingRepository bookingRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void databaseHasCustomers(){
+		List<Customer> foundCustomer = customerRepository.findAll();
+		assertEquals(2, foundCustomer.size());
 	}
 
 }
